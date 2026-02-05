66b9c3e8a6e18e0b2336ab0a4b78072162fcabe9
Regan <regan.hart2@gmail.com>
2026-02-05 17:57:13 +0000

    Added file via upload
    Updated so gluten free alternatives are flagged separately and will not raise a false negative in a gluten word is listed.

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 def check_ingredients(ingredients):
     checking = ingredients.lower()
     gluten = ["wheat", "flour", "barley", "rye", "oats", "triticale", "malt"]
-    if any(word in checking for word in gluten):
+    gluten_free = ["gluten free", "gluten-free"]
+    if any(word in checking for word in gluten_free):
+        print("This product contains gluten free alternatives - enjoy!")
+    elif any(word in checking for word in gluten):
         print("This product contains gluten - stay away!")
     else:
         print("This product does not contain gluten - enjoy!")
 
-check_ingredients("Insert list of ingredients as a string")
+check_ingredients("<<<Insert list of ingredients here as a string>>>")
+
+# test cases
+# check_ingredients("wheat")
+# check_ingredients("gluten free flour")
+# check_ingredients("cabbage")
